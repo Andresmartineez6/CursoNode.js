370641674f64bf88579fc6a6db0701d91d66c74f
Andresmartineez6 <andres.martinez@impulsatelecom.com>
2025-05-21 10:38:07 +0200

    Muestro los logs de los test

--- src/infrastructure/repositories/inMemoryUserRepo.ts
+++ src/infrastructure/repositories/inMemoryUserRepo.ts
@@ -5,18 +5,22 @@ export class InMemoryUserRepo implements EmployeeRepository {
   private employees: Employee[] = [];
 
   async findById(id: string): Promise<Employee | null> {
+    console.log(`Buscando empleado con ID: ${id}`);
     return this.employees.find((employee) => employee.getId() === id) || null;
   }
 
   async findAll(): Promise<Employee[]> {
+    console.log(`Número total de empleados: ${this.employees.length}`);
     return this.employees;
   }
 
   async create(employee: Employee): Promise<void> {
+    console.log(`Creando empleado: ${employee.getName()}`);
     this.employees = [...this.employees, employee];
   }
 
   async update(employeeParam: Employee): Promise<void> {
+    console.log(`Actualizando empleado con ID: ${employeeParam.getId()}`);
     const employee = this.employees.find(
       (employee) => employee.getId() === employeeParam.getId(),
     );
@@ -32,6 +36,7 @@ export class InMemoryUserRepo implements EmployeeRepository {
   }
 
   async delete(id: string): Promise<void> {
+    console.log(`Eliminando empleado con ID: ${id}`);
     this.employees = this.employees.filter(
       (employee) => employee.getId() !== id,
     );
@@ -59,6 +64,8 @@ export class InMemoryUserRepo implements EmployeeRepository {
       position: 'junior' | 'senior' | 'teamLeader' | 'ceo';
     }>,
   ) {
+    console.log(`Filtrando empleados con: ${JSON.stringify(filter)}`);
+
     return this.employees.filter((user) => {
       // pones el ternario a true, porque si no recibes el filtro de name, pones a true para que pasen todos los names
       const matchName = filter.name
